1e8213d6e65ecde608fbe11734ddcc060ecb179c
cmanzana <carlos.manzanares+github@gmail.com>
2011-06-07 09:10:15 +0300

    adding lonely planet app

---
+++ lonely-planet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LonelyPlanet::Application.config.secret_token = '593eec4e63bc660a40d09ecaf729812dcd2ebf205c6d82d264436e27dd0a0e4d419ab07badd09f5796c1bc079af648f8b18a914b1732f9f62e753cbb99a497b8'
