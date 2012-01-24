0a2aac8e0b2407e49bde4940d990398170eab8df
Lukasz Jastrzebski <lukasz.jastrzebski@gmail.com>
2012-01-24 14:36:45 -0800

    repository moved

--- sms-feeder/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-HelloWorld::Application.config.secret_token = '358b5df6188dfa5623027c2bf9ed7b0d1d533bb4d78bc1e87c3befe140571fc7802082ebfd684769e34941b9de60f49cb60a924205692cea5cb75353a292dfa2'
